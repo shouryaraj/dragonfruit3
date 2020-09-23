1829799ad8a7b2b8be2e16322dab2f708f235fda
dpham06 <dpha0005@student.monash.edu>
2020-09-23 19:09:49 +1000

    Add Evergreen, change index.js, fix css
    Add Evergreen dependency
    Add popover and new cards to index.js
    Fix sizing for some cards

--- src/RadarChart.js
+++ src/RadarChart.js
@@ -1,44 +1,44 @@
-import React , {Component} from "react";
-import {Radar} from 'react-chartjs-2';
+import React, { Component } from "react";
+import { Radar } from 'react-chartjs-2';
 
-class RadarChart extends Component{
-    constructor(props){
+class RadarChart extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             data: {
                 labels: ["Confidence Rating", "Content Completeness", "Learning Confidence", "Content Positivity", "Discussion Confidence", "Workload Management"],
                 datasets: [{
                     label: ["Past Weeks"],
-                    data: [4.5,2,4,3,2,5],
+                    data: [4.5, 2, 4, 3, 2, 5],
                     backgroundColor: 'rgba(197, 250, 243,0.2)',
                     borderColor: '#30e3ca',
                     borderWidth: 2,
                 },
                 {
                     label: ["Current Week"],
-                    data: [3,4,5,4,1,5],
+                    data: [3, 4, 5, 4, 1, 5],
                     backgroundColor: 'rgba(47, 137, 252,0.2)',
                     borderColor: '#2f89fc',
                     borderWidth: 2,
                 }],
-            } ,
+            },
         }
 
     }
 
-    render(){
-        return(
-            <div className = "radarChart" style= {{width: "650px", height: "350px"}}>
+    render() {
+        return (
+            <div className="radarChart" style={{ width: "100%", height: "100%" }}>
                 <Radar
                     data={this.state.data}
-                    options={{ 
-                        responsive:true,
+                    options={{
+                        responsive: true,
                         maintainAspectRatio: false,
                         gridLines: {
                             display: true,
                             color: "#ffffff"
                         },
-                        legend:{
+                        legend: {
                             display: true,
                             position: 'right',
                             fontColor: "#ffffff",
@@ -59,9 +59,9 @@ class RadarChart extends Component{
                             },
                             gridLines: {
                                 color: 'grey'
-                              }
+                            }
                         }
-                        
+
                     }}
                 />
             </div>
